488e7be3347ae085fb1d857625d76a24c2548d01
Peter Karth <falonofthetower@gmail.com>
2017-02-27 16:07:19 -0500

    filter my-assignments by finished

--- app/routes/assignments.js
+++ app/routes/assignments.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('my-assignment', {include: 'assignee,assigner,todo'});
+    return this.get('store').query('my-assignment', {
+      include: 'assignee,assigner,todo',
+      filter: {
+        finished: false
+      }
+    });
   },
 });
