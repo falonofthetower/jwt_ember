31503823e25f6568331a78818b2405fd8933d952
Peter Karth <falonofthetower@gmail.com>
2017-03-01 21:22:35 -0500

    setup for production, cleaned cruft

--- app/models/assignment.js
+++ app/models/assignment.js
@@ -1,9 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  // assigner: DS.belongsTo('user', { async: true }),
-  // assignee: DS.belongsTo('user', { async: true }),
-  // todo: DS.belongsTo('todo', { async: true })
   assigner: DS.belongsTo('user', { async: true }),
   assignee: DS.belongsTo('user', { async: true }),
   todo: DS.belongsTo('todo', { async: true }),
