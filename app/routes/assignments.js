dc5949f73079d09ab3d40b7d9b60bf6695d5bd1f
Peter Karth <falonofthetower@gmail.com>
2017-03-27 01:29:55 -0400

    Redirecting on unauthorized routes
    - Routes set to redirect if unauthorized
    - Previously opened but failed fetching data

--- app/routes/assignments.js
+++ app/routes/assignments.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
     return this.get('store').query('my-assignment', {
       include: 'assignee,assigner,todo',
