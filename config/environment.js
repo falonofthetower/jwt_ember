985c7c423270f900c2e0b4fd37789aa4cd2b8e47
Peter Karth <falonofthetower@gmail.com>
2017-04-02 11:31:48 -0400

    make it SSL

--- config/environment.js
+++ config/environment.js
@@ -39,7 +39,7 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     ENV.APP.host = "http://localhost:3000";
-    ENV.APP.websocket_host = "ws://localhost:3000/websocket";
+    ENV.APP.websocket_host = "wss://localhost:3000/websocket";
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
