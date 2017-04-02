4e2d4d01be88c13072e42762b67b167f77062324
Peter Karth <falonofthetower@gmail.com>
2017-04-02 11:29:10 -0400

    importing config to set websocket host

--- app/controllers/requests.js
+++ app/controllers/requests.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
+import config from 'frontend/config/environment';
 
 export default Ember.Controller.extend({
   cableService: Ember.inject.service('cable'),
 
   setupSubscription: Ember.on('init', function() {
-    var consumer = this.get('cableService').createConsumer(ENV.APP.websocket_host);
+    var consumer = this.get('cableService').createConsumer(config.APP.websocket_host);
     var subscription = consumer.subscriptions.create("AssignmentsChannel", {
       received: (data) => {
         this.get('store').findRecord('my-request', data.id).then(function(assignment) {
