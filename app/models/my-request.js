898d186eef35ccd0f7e762707dc5da3dba78a6f2
Peter Karth <falonofthetower@gmail.com>
2017-04-02 14:11:06 -0400

    cleaning up

--- app/models/my-request.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  assigner: DS.belongsTo('user', { async: true }),
-  assignee: DS.belongsTo('user', { async: true }),
-  todo: DS.belongsTo('todo', { async: true }),
-  status: DS.attr('string')
-});
